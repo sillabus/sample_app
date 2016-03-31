3217d1d2d09a060affc53583bf93fbbd38047a33
Roger Tafoya <sillabus@aol.com>
2016-03-31 13:41:23 -0700

    updated html/css

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,25 +4,25 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
+    assert_select "title", "You've just entered another dimension"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Help | You've just entered another dimension"
   end
 
   test "should get about" do
   	get :about
   	assert_response :success
-  	assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+  	assert_select "title", "About | You've just entered another dimension"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
-    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Contact | You've just entered another dimension"
   end
 
 end
